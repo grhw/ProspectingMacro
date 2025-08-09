f17e3211315271f6886a65a66bf3fa95403ae053
Gust Who? <lucgustga@outlook.com>
2025-08-09 17:15:21 +0800

    a lil formatting here

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 from time import sleep
-from mss import mss
 from PIL import Image
+from mss import mss
+import pytesseract
 import pynput
 import time
-import pytesseract
 
-mouse = pynput.mouse.Controller()
 keyboard = pynput.keyboard.Controller()
+mouse = pynput.mouse.Controller()
 
 coords = {
-    "progress": {"left": 1522+1920, "top": 608, "width": 100, "height": 531},
-    "action_text": {"left": 881+1920, "top": 1255, "width": 802, "height": 72},
-    "bar": {"left": 960+1920, "top": 1143, "width": 802, "height": 2}
+    "action_text": {"left": 881 + 1920, "top": 1255, "width": 802, "height": 72},
+    "progress": {"left": 1522 + 1920, "top": 608, "width": 100, "height": 531},
+    "bar": {"left": 960 + 1920, "top": 1143, "width": 802, "height": 2},
 }
 
+
 class Modes:
-    moving = "Moving"
     digging = "Collect Deposit"
+    moving = "Moving"
     shaking = "Pan"
+
 
 tes_config = "--psm 7 --user-patterns tes_modes.txt"
 ss = mss()
 
+
 def is_color(rgb, ref, leniency=15):
-    return sum(abs(a-b) for a, b in zip(rgb, ref)) < leniency
+    return sum(abs(a - b) for a, b in zip(rgb, ref)) < leniency
+
 
 def to_pil(sct_img):
     return Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
 
+
 mode = Modes.moving
 nothing_count = 15
 fill_count = 5
+
 
 def update_mode():
     global mode, nothing_count, fill_count
@@ -55,10 +61,8 @@ def update_mode():
         print("mode:", mode)
         print("text:", text.strip())
 
-clocks = {
-    "dig_cooldown": time.time(),
-    "dig_duration": time.time()
-}
+
+clocks = {"dig_cooldown": time.time(), "dig_duration": time.time()}
 
 is_using_w = True
 
@@ -67,9 +71,11 @@ while True:
     if mode == Modes.digging:
         if time.time() - clocks["dig_cooldown"] > 3:
             progress = to_pil(ss.grab(coords["progress"]))
-            hit = is_color(progress.getpixel((13, 19)), (255, 255, 255)) or is_color(progress.getpixel((13, 8)), (255, 255, 255))
+            hit = is_color(progress.getpixel((13, 19)), (255, 255, 255)) or is_color(
+                progress.getpixel((13, 8)), (255, 255, 255)
+            )
 
-            if not is_color(progress.getpixel((77,516)),(140,140,140)):
+            if not is_color(progress.getpixel((77, 516)), (140, 140, 140)):
                 fill_count -= 1
             if fill_count < 0:
                 mode = Modes.moving
@@ -106,4 +112,4 @@ while True:
     if mode != Modes.digging:
         update_mode()
 
-    sleep(1/144)
+    sleep(1 / 144)
